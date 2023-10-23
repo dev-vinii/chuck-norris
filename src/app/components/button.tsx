10bd5ce37d3aa6771c0b6daef43a5767ec80d7b8
Vinicius Luna <129511403+VniciNN@users.noreply.github.com>
2023-10-23 03:13:41 +0000

    Update button.tsx and page.tsx

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,13 +1,15 @@
 export function GenerateJoke() {
-    return (
-        <div className="h-14 bg-custom-button w-72 text-center flex items-center justify-center rounded-3xl tablet:mb-10 desktop:mt-10 laptop:mt-10 desktop:mb-10 laptop:mb-10 text-xl cursor-pointer">Generate New Joke</div>
-    )
+  return (
+    <div className="h-14 bg-custom-button w-72 text-center flex items-center justify-center rounded-3xl tablet:mb-10 desktop:mt-10 laptop:mt-10 desktop:mb-10 laptop:mb-10 text-xl cursor-pointer">
+      Generate New Joke
+    </div>
+  );
 }
 
 export function AddFavs() {
-    return (
-        <div className="h-14 bg-custom-button w-72 text-center flex items-center justify-center rounded-3xl tablet:mb-10 desktop:mt-10 laptop:mt-10 desktop:mb-10 laptop:mb-10 text-xl cursor-pointer">
-            Add Favorites
-        </div>
-    )
+  return (
+    <div className="h-14 bg-custom-button w-72 text-center flex items-center justify-center rounded-3xl tablet:mb-10 desktop:mt-10 laptop:mt-10 desktop:mb-10 laptop:mb-10 text-xl cursor-pointer">
+      Add Favorites
+    </div>
+  );
 }
