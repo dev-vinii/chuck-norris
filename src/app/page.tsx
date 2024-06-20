99a8c90c7e9b6b53e04b7fd328993081c983c0f2
Vinicius <viniciusluna@yahoo.com>
2024-06-19 23:17:16 -0300

    feat: update principal page

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useJoke } from "./hooks/useJoke";
+import FavoriteList from "./components/FavoriteList/FavoriteList";
+import JokeContainer from "./components/JokeContainer/JokeContainer";
+
+export default function Jokes() {
+  const useJokes = useJoke();
+  const { joke, favorites, show, handleFavorite, deleteFavorite, getJoke } = useJokes;
+  return (
+    <div>
+      <div className="text-violet-300 text-center font-bold mt-10 text-3xl">
+        Chuck Norris
+        <JokeContainer joke={joke} show={show} handleFavorite={handleFavorite} getJoke={getJoke} />
+        <FavoriteList favorites={favorites} deleteFavorite={deleteFavorite}/>
+      </div>
+    </div>
+  );
+}
